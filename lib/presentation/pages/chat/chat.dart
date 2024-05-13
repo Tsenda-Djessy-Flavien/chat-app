4fc8e317fc2882758c5f96d29171115ae9dbe5c1
djesstsenda <djesstsenda@gmail.com>
2024-05-13 15:57:13 +0100

    feat(main):Requesting Permissions & Getting an Address Token

--- lib/presentation/pages/chat/chat.dart
+++ lib/presentation/pages/chat/chat.dart
@@ -1,10 +1,31 @@
 import 'package:chat_app/presentation/widgets/chat_message.dart';
 import 'package:chat_app/presentation/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotification() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    // recuperer le token de l'appareil qui envoie la notification
+    final token = await fcm.getToken();
+    // vous pouvez envoyer ce token via http ou bien un sdk firestore (backend)
+    print('device notif token: $token');
+  }
+
+  @override
+  void initState() {
+    setupPushNotification();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
